31769f5c4b79df364fa8ead16c29d0a87fdec900
Leon-Paul Hart <leon.paul.hart@gmail.com>
2020-05-20 15:44:19 +0100

    initial commit - set up new console app and .gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OMDb_API_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my Open Movie Database API Explorer Console App");
+        }
+    }
+}
