e9c525e54d64bde4e1c6f161a3a3221b88a3f39d
Leon-Paul Hart <24394216+leon-paul-hart@users.noreply.github.com>
2021-02-17 11:25:59 +0000

    Added OMDb Service Interface

---
+++ ClassLibraries/OMDb_API_Service/IOMDb_API_Service.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMDb_API_Service
+{
+    interface IOMDb_API_Service
+    {
+    }
+}
