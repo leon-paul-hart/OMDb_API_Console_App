26dde4ec2904016cc35e69adc5cc1d2a12ac1b4c
Leon-Paul Hart <24394216+leon-paul-hart@users.noreply.github.com>
2021-02-17 11:25:28 +0000

    Added API Key Interface

---
+++ ClassLibraries/API_Key_Service/IAPI_Key_Service.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_Key_Service
+{
+    interface IAPI_Key_Service
+    {
+    }
+}
