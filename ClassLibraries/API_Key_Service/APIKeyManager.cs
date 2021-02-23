7f06ecd8c48d11b65b0937b4d53a9b8894a1cab2
Leon-Paul Hart <leon.paul.hart@protonmail.com>
2021-02-23 16:44:50 +0000

    Added TODO notes

--- ClassLibraries/API_Key_Service/APIKeyManager.cs
+++ ClassLibraries/API_Key_Service/APIKeyManager.cs
@@ -5,6 +5,7 @@ namespace API_Key_Service.APIKeyManager
         #region Properties
         
         private string API_Key {get; set;}
+        // TODO Move API_Key to a POCO with Validation Attributes
         
         #endregion
 
@@ -25,6 +26,12 @@ namespace API_Key_Service.APIKeyManager
         public void SetAPIKey(string new_Key_Value){
             API_Key = new_Key_Value;
         }
+
+        //TODO 
+
+        // Add Method to write API_Key to file
+
+        // Add Method to get API_Key from file
 
         #endregion
     }
