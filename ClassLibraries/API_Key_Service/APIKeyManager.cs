fad0c76ac0f0bf36adf04d656b04841bf4dac188
Leon-Paul Hart <leon.paul.hart@protonmail.com>
2021-02-23 13:50:50 +0000

    Minor formatting changes - added #regions

--- ClassLibraries/API_Key_Service/APIKeyManager.cs
+++ ClassLibraries/API_Key_Service/APIKeyManager.cs
@@ -2,12 +2,22 @@ namespace API_Key_Service.APIKeyManager
 {
     public class APIKeyManager
     {
+        #region Properties
+        
         private string API_Key {get; set;}
+        
+        #endregion
 
+        #region Constructors
+        
         public APIKeyManager(string Key_Value){
             API_Key = Key_Value;
         }
+        
+        #endregion
 
+        #region Public Methods
+        
         public string GetAPIKey(){
             return API_Key;
         }
@@ -15,5 +25,7 @@ namespace API_Key_Service.APIKeyManager
         public void SetAPIKey(string new_Key_Value){
             API_Key = new_Key_Value;
         }
+
+        #endregion
     }
 }
