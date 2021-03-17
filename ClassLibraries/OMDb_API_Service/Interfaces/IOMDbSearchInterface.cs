e4cc825e83c43919823311dcb0e0532d70a89e4f
Leon-Paul Hart <leon.paul.hart@protonmail.com>
2021-03-17 00:17:35 +0000

    Added Interface for OMDB

---
+++ ClassLibraries/OMDb_API_Service/Interfaces/IOMDbSearchInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMDb_API_Service.Interfaces.IOMDbSearchInterface
+{
+    interface IOMDbSearchInterface
+    {
+        void SearchByIMDbId(string id);
+
+        void SearchByMovieTitle(string title);
+    }
+}
