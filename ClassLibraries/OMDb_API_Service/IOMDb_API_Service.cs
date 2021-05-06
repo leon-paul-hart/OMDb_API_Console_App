fe3fb973127ae24702b3d0043eea5d76a653d007
Leon-Paul Hart <leon.paul.hart@protonmail.com>
2021-05-06 18:20:22 +0100

    Removed and sorted usings

--- ClassLibraries/OMDb_API_Service/IOMDb_API_Service.cs
+++ ClassLibraries/OMDb_API_Service/IOMDb_API_Service.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OMDb_API_Service
+﻿namespace OMDb_API_Service
 {
     interface IOMDb_API_Service
     {
